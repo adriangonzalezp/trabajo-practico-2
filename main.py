21b7fb35561da0f7778f0fa70943b39fdae0dc78
Adrian Gonzalez <106283798+adriangonzalezp@users.noreply.github.com>
2025-02-14 16:27:06 -0600

    se agrego la funcion de agregar usuario usando POST

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# lista para guardar los usuarios
+users = []
+
+# modelo de usuario
+class User(BaseModel):
+    name: str
+    email: str
+
+# endpoint usando POST para agregar usuario
+@app.post("/add-user/")
+def add_user(user:User):
+    users.append(user)
+    return {"message": "Usuario registrado con éxito", "user": user}
+
+# endpoint usando GET para traer los usuarios
+@app.get("/get-users/")
+def get_users():
+    return {"users": users}
+
+
+
+
+
